e3ba63a731b15672adf9bdac799f33a445dbbb4c
Anton <antonio.fiasco24@gmail.com>
2024-11-21 10:01:53 +0000

    create-wishlist-endpoiint

--- aml/server/wishlist-service/src/controllers/wishlistController.js
+++ aml/server/wishlist-service/src/controllers/wishlistController.js
@@ -1,3 +1,37 @@
 const WishlistRecord = require('../models/wishlist');
 
+// Create a new wishlist record
+exports.createWishlist = (req, res) => {
+    const wishlist = new WishlistRecord({
+        userId: req.params.userId,
+        mediaId: req.params.mediaId
+    });
+
+    wishlist.save()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "An error occurred while creating the wishlist record."
+            });
+        });
+};
+
+// Get all wishlist records by user ID
+exports.getWishlistByUserId = (req, res) => {
+    WishlistRecord.find({ userId: req.params.userId })
+        .then(wishlist => {
+            // Make request to inventory-service to get media data for each media ID
+            
+
+            res.send(wishlist);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "An error occurred while retrieving the wishlist records."
+            });
+        });
+};
+
 
--- aml/server/wishlist-service/src/routes/wishlistRoutes.js
+++ aml/server/wishlist-service/src/routes/wishlistRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const wishlistController = require('../controllers/wishlistController');
 
+router.post('/user/:userId/media/:mediaId', wishlistController.createWishlist);
+router.get('/user/:userId', wishlistController.getWishlistByUserId);
 // router.post('/', mediaController.createMedia);
 // router.get('/', mediaController.getMedia);
 // router.get('/:id', mediaController.getMediaById);
