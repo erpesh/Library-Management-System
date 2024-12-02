988d5fc952a59a700d8ed47d0f4d04b215ef8907
Anton <antonio.fiasco24@gmail.com>
2024-12-02 12:08:54 +0000

    backend typos

--- aml/server/wishlist-service/src/controllers/wishlistController.js
+++ aml/server/wishlist-service/src/controllers/wishlistController.js
@@ -1,15 +1,11 @@
 const WishlistRecord = require('../models/wishlist');
 
 // Create a new wishlist record
-exports.createWishlist = (req, res) => {
+exports.createWishlistRecord = (req, res) => {
     const wishlist = new WishlistRecord({
         userId: req.params.userId,
         mediaId: req.params.mediaId
     });
-
-    console.log('asfafas')
-    console.log(req.params.userId, req.params.mediaId)
-    console.log(wishlist);
 
     wishlist.save()
         .then(data => {
@@ -27,7 +23,7 @@ exports.getWishlistByUserId = async (req, res) => {
     try {
         const wishlist = await WishlistRecord.find({ userId: req.params.userId });
 
-        if (!wishlist || wishlist.length === 0) {
+        if (!wishlist) {
             return res.status(404).send({ message: "No wishlist records found for this user." });
         }
 
@@ -42,7 +38,7 @@ exports.getWishlistByUserId = async (req, res) => {
 
 // Delete a single wishlist record by ID
 exports.deleteWishlistRecordById = (req, res) => {
-    WishlistRecord.findByIdAndDelete(req.params.Id)
+    WishlistRecord.findByIdAndDelete(req.params.id)
         .then(wishlist => {
             if (!wishlist) {
                 return res.status(404).send({ message: "Wishlist record not found." });
