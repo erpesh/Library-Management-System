c5bcb5cd5069dee35c4cfcf742cb4a4acd0b1691
sansgitt <hanacham1118@gmail.com>
2024-12-17 18:32:09 +0000

    test

--- aml/server/wishlist-service/src/controllers/wishlistController.test.js
+++ aml/server/wishlist-service/src/controllers/wishlistController.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-const app = require('../app'); // One level back to src
-const Wishlist = require('../models/wishlist');  // Assuming the Wishlist model is in the models directory
+const app = require('../app'); 
+const Wishlist = require('../models/wishlist');  
 
 let mongoServer;
 
